f129da6f93cd7aa9be1bb4d0b97b825d7009999b
orelpa <pa.orel@yandex.ru>
2024-04-25 09:16:30 +0300

    Исправлены опечатки

--- config_data/config.py
+++ config_data/config.py
@@ -9,7 +9,7 @@ class TgBot:
 class Config:
     tg_bot: TgBot
 
-def load_config(path: str | None = Npne) -> Config:
+def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
